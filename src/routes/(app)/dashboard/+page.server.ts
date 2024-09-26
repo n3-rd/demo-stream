cd0420b7e7b5b9f777f1dc034c0426d134a875b3
Godwin Jemegah <godwinjdayo@gmail.com>
2024-09-26 22:50:19 +0100

    rooom creation

--- src/routes/(app)/dashboard/+page.server.ts
+++ src/routes/(app)/dashboard/+page.server.ts
@@ -3,10 +3,11 @@ import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
     const user = locals.pb.authStore.model;
+    const rooms = await locals.pb.collection('rooms').getFullList();
 
     if (!user) {
         throw redirect(302, '/login');
     }
 
-    return { user };
+    return { user, rooms };
 };

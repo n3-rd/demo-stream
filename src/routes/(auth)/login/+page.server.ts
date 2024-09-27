8abf9e49eec5916fce21e2abd10b9d5ac8ea1fbe
Godwin Jemegah <godwinjdayo@gmail.com>
2024-09-27 14:04:26 +0100

    solved login incorrect auth issue

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,15 +1,15 @@
-// import { error, redirect } from '@sveltejs/kit';
-export const actions = {
+import type { Actions } from './$types';
+
+export const actions: Actions = {
 	login: async ({ request, locals }) => {
 		const body = Object.fromEntries(await request.formData());
 
 		try {
-			await locals.pb.collection('users').authWithPassword(body.email, body.password);
+			await locals.pb.collection('users').authWithPassword(body.email as string, body.password as string);
+			return { success: true, message: 'Logged in successfully' };
 		} catch (err) {
 			console.log('Error: ', err);
-			return { success: false, message: 'Something went wrong logging in' };
+			return { success: false, message: 'Invalid email or password' };
 		}
-
-		return { success: true, message: 'logged in successfully' };
 	}
 };

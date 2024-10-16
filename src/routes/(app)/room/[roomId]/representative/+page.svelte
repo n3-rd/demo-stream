ea2723c13a68531432e97bf4938aceba834bff59
Godwin Jemegah <godwinjdayo@gmail.com>
2024-10-16 15:55:21 +0100

    new rep route

---
+++ src/routes/(app)/room/[roomId]/representative/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let data;
+  let { representative } = data.representative;
+</script>
+
+{#if representative}
+ redirecting to meeting.....
+{:else}
+  <p>You are not a representative, hence you cannot access through this  page</p>
+{/if}

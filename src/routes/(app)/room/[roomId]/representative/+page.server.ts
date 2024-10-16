879a0a59bfe7dc6190f099d428acf38ca2d9c43d
Godwin Jemegah <godwinjdayo@gmail.com>
2024-10-16 15:55:21 +0100

    new rep route

---
+++ src/routes/(app)/room/[roomId]/representative/+page.server.ts
@@ -0,0 +1,20 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals, params  }) => {
+      if (!locals.pb.authStore.isValid) {
+        throw redirect(302, '/login');
+    }
+
+    const user = locals.pb.authStore.model;
+    const representative = user.representative
+    if (representative) {
+        throw redirect(302, `/room/${params.roomId}`);
+    }
+    console.log(user)
+    return {
+        user,
+        params,
+        representative
+    };
+};

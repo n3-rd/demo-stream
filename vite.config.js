ad2fa026089c6932e0765354f621b41401564a58
n3-rd <godwinjdayo@gmail.com>
2025-01-21 13:10:15 +0100

    media selector header fix & vite polling

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+		  usePolling: true,
+		  interval: 300, // Adjust polling interval (milliseconds)
+		}
+	  }
 };
 
 export default config;

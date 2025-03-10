314788e24924d8880e811923b24b6978b4d30866
n3-rd <godwinjdayo@gmail.com>
2025-03-10 15:44:37 +0100

    page fixes and content library carousel

--- src/routes/(app)/room/[roomId]/info/+page.svelte
+++ src/routes/(app)/room/[roomId]/info/+page.svelte
@@ -109,7 +109,7 @@
     <Sidenav activePage="rooms" />
     
     <div class="flex-1 ">
-        <div class="max-w-[1115px] mx-auto p-6 space-y-6">
+        <div class=" mx-auto p-6 space-y-6">
             <!-- Header -->
             <div class="bg-white rounded-[8px] h-[69px] flex items-center justify-between px-6">
                 <h1 class="font-['Poppins'] text-[24px] font-bold leading-[118%] text-[#808080]">{room?.title || ''}</h1>

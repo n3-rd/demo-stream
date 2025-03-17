2476131735a0f90a54297e88c9773ee8eaf4666f
n3-rd <godwinjdayo@gmail.com>
2025-03-17 13:25:55 +0100

    adapter fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// Use Node.js runtime
-			runtime: 'nodejs18.x',
-		}),
+		adapter: adapter(),
 		csrf: {
 			checkOrigin: false,
 		},

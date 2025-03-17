0e96c97f5f37b026c2bce1f8188f900ce45c6a02
n3-rd <godwinjdayo@gmail.com>
2025-03-17 12:47:16 +0100

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// Use Node.js runtime
+			runtime: 'nodejs18.x',
+		}),
 		csrf: {
 			checkOrigin: false,
 		},
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ const config = {
 			interval: 300, // Adjust polling interval (milliseconds)
 		},
 		hmr: {
-			host: 'viewroom.ca', // Ensure it matches your deployed domain
+			host: 'localhost', // Ensure it matches your deployed domain
 			protocol: 'wss', // Use secure WebSockets
 			clientPort: 443, // Use standard HTTPS port for client connections
 		},

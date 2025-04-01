5af8023af52a8b3c6051a416f367611851c1bb78
n3-rd <godwinjdayo@gmail.com>
2025-04-01 16:27:17 +0100

    ui fixes

--- src/lib/components/ui/dialog/dialog-overlay.svelte
+++ src/lib/components/ui/dialog/dialog-overlay.svelte
@@ -16,6 +16,6 @@
 <DialogPrimitive.Overlay
 	{transition}
 	{transitionConfig}
-	class={cn("bg-background/80 fixed inset-0 z-50 backdrop-blur-sm", className)}
+	class={cn("bg-[#2f2f2fe6]/90 fixed inset-0 z-50 ", className)}
 	{...$$restProps}
 />

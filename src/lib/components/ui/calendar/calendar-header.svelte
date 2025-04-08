93b0abdadb1ba4f1ced0437435126441cd5021d4
n3-rd <godwinjdayo@gmail.com>
2025-04-08 15:07:04 +0100

    Update left-bar and schedule-meeting components: Adjusted dialog width in left-bar and added additional information field in schedule-meeting. Enhanced error handling with toast notifications for various actions in AI Room Designer.

--- src/lib/components/ui/calendar/calendar-header.svelte
+++ src/lib/components/ui/calendar/calendar-header.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <CalendarPrimitive.Header
-	class={cn("relative flex w-full items-center justify-between pt-1", className)}
+	class={cn("relative flex w-fit items-center gap-10 mx-auto pt-1", className)}
 	{...$$restProps}
 >
 	<slot />
